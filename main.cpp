ccc3c5f6c9255e3830398907e72d8007fbcad69b
DRABCZUK Barthelemy <d17010509@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-11 17:31:16 +0100

    exo 1/2 des tp boost

--- main.cpp
+++ main.cpp
@@ -1,213 +1,75 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
-#include <cstdio>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/barrier.hpp>
+
+#include <unistd.h>             // sleep()
+
+#include "SleepChronogram.h"
 
 using namespace std;
+using namespace boost;
+using namespace boost::posix_time;
+using namespace nsUtil;
 
-/**exo1**/
-void Fct () {
-    cout << "Hello World!" << endl;
-    cout << "bonjour" << flush;
-    cout << " tout" << flush << " le monde" << endl;
+
+barrier* p_ma_barrier;
+void RendezVous () {
+    //cout << "waiting ..." << endl;
+    p_ma_barrier->wait();
+    //cout << "finish." << endl;
 }
 
-void exo1 () {
-    cout << "Ma machine a : " << thread::hardware_concurrency() << " coeurs" << endl;
-}
-
-/**exo2**/
-void exo2 () {
-    thread corobizar (Fct);
-    corobizar.join();
-    thread sardoche (Fct);
-    sardoche.join();
-}
-
-/**exo3**/
-void exo3 () {
-    thread corobizar (Fct);
-    cout << "id = " << corobizar.get_id() << endl;
-    corobizar.join();
-}
-
-/**exo4**/
-void exo4 () {
-    thread corobizar (Fct);
-    cout << "id = " << corobizar.get_id() << endl;
-    corobizar.join();
-    thread sardoche (Fct);
-    cout << "id = " << sardoche.get_id() << endl;
-    sardoche.join();
-}
-
-/**exo5**/
-void exo5 () {
-    thread corobizar (Fct);
-    cout << "id = " << corobizar.get_id() << endl;
-    corobizar.join();
-    thread sardoche (Fct);
-    cout << "id = " << sardoche.get_id() << endl;
-    sardoche.join();
-}
-
-/**exo6**/
-mutex mutIO;
-void fct2 (unsigned & i) {
-    cout << "id : " << this_thread::get_id() << endl;
-    mutIO.lock ();
-    cout << "thread numero " << ++i << endl;
-    mutIO.unlock ();
-}
-
-mutex insec;
-void exo6 (unsigned & i) {
-    thread corobizar (fct2, ref (i));
-    thread sardoche (fct2, ref (i));
-    insec.lock();
-    cout << "corobizar : " << corobizar.get_id() << endl;
-    insec.unlock();
-    corobizar.join();
-    insec.lock();
-    cout << "sardoche : " << sardoche.get_id() << endl;
-    insec.unlock();
-    sardoche.join();
-}
-
-/**exo7**/
-void exo7 () {
-    vector<thread> mesFreds;
-    unsigned i = 0;
-    mesFreds.push_back(thread (fct2, ref(i)));
-    mesFreds.push_back(thread (fct2, ref(i)));
-    mesFreds.push_back(thread (Fct));
-
-    for (thread & monFred : mesFreds) {
-        monFred.join ();
-    }
-}
-
-/**exo8**/
-mutex tex;
-unsigned i (0);
-void inc () {
-    tex.lock();
-    ++i;
-    cout << i << endl;
-    tex.unlock();
-}
-
-void exo8 () {
-    vector <thread> mesFreds;
-
-    for (unsigned i (0); i < 10; ++i)
-        mesFreds.push_back(thread (inc));
-
-    for (thread & monFred : mesFreds) {
-        monFred.join();
-    }
-
-}
-
-/**exo9**/
-vector<unsigned> createVector (unsigned size, vector<unsigned> & myVec) {
-    srand (time (NULL));
-    for (unsigned (0); i < size; ++i) {
-        myVec.push_back((unsigned)rand()%500);
-    }
-    return myVec;
-}
-
-void printVector (vector<unsigned> myVec) {
-    for (unsigned i : myVec)
-        cout << i << endl;
-}
-
-void exo9 () {
-    vector<unsigned> myVec;
-
-    thread sardoche (createVector, 10, ref(myVec));
-    sardoche.join();
-    thread corobizar (printVector, myVec);
-    corobizar.join();
-}
-
-/**exo10**/
-enum Etat {PENSER, MANGER, A_FAIM};
 mutex mut;
-vector<shared_ptr<mutex>> dodo;
-vector<Etat> etat_des_philo;
 
-void test_manger (unsigned i){
-    if (etat_des_philo [(i+1) % etat_des_philo.size()] != MANGER &&
-            etat_des_philo [(i-1) % etat_des_philo.size()] != MANGER &&
-            etat_des_philo [i] == A_FAIM) {
-        etat_des_philo [i] = MANGER;
-        cout << "philosophe : " << i << endl;
-        cout << "Je mange." << endl;
-        dodo [i]->unlock();
+class CPassant {
+
+   //id
+    static unsigned id;
+    unsigned myId;
+
+    // times
+    unsigned time_to_arrive;
+    unsigned time_to_go;
+
+public:
+
+    CPassant (unsigned timeArr, unsigned timeDep) :  myId (++id) , time_to_arrive (timeArr), time_to_go (timeDep) {}
+
+    void operator () (void) {
+        unsigned Col = 1;
+        nsUtil::SleepChronogram (this->myId, Col, '-', this->time_to_arrive, mut);
+        ::RendezVous ();
+        nsUtil::SleepChronogram (this->myId, Col, '_', this->time_to_go, mut);
+        nsUtil::SleepChronogram (this->myId, Col, 'X', 1, mut);
     }
-}
 
-void prendre (unsigned i) {
-    lock_guard<mutex> lock (mut);
-    cout << "philosophe : " << i << endl;
-    etat_des_philo[i] = A_FAIM;
-    cout << "J'ai faim." << endl;
-    test_manger(i);
-    dodo[i]->lock();
-}
+}; // CPassant
 
-void poser(unsigned i) {
-    lock_guard<mutex> lock (mut);
-    etat_des_philo [i] = PENSER;
-    cout << "philosophe : " << i << endl;
-    cout << "Je pense." << endl;
-    test_manger((i+1) % etat_des_philo.size());
-    test_manger((i-1) % etat_des_philo.size());
-}
+unsigned CPassant::id = 0;
 
-void exo10 () {
+int main () {
 
-    vector<thread> philo_qui_ont_faim;
-    vector<thread> philo_qui_n_ont_plus_faim;
+    thread_group mesPassants;
 
-    /*****************************/
-    /**** INITIALISE LES TABS ****/
-    /****       ABRUTI        ****/
-    /*****************************/
+    unsigned nb_threads, temps_avant_rdv, temps_apres_rdv (0);
 
-    #pragma openmp
-    for (unsigned i (0); i < 10; ++i)
-        etat_des_philo.push_back(PENSER);
+    //cout << "entrer un nombre de threads a lancer : ";
+    cin >> nb_threads;
 
-    #pragma openmp
-    for (int i (0); i < 10; ++i)
-        dodo.push_back(shared_ptr<mutex> (new mutex()));
+    p_ma_barrier = new barrier (nb_threads);
 
-    #pragma openmp
-    for (unsigned i (0); i < 10; ++i)
-        philo_qui_ont_faim.push_back(thread (prendre, i));
+    cout << clrscr;
+    for (unsigned i (0); i <  nb_threads; ++i) {
+        //cout << "entrer le delais d'arrivee : ";
+        cin >> temps_avant_rdv >> temps_apres_rdv;
+        //cout << "entrer le delais de depart : ";
 
-    #pragma openmp
-    for (unsigned i (0); i < 10; ++i)
-        philo_qui_n_ont_plus_faim.push_back(thread (poser, i));
+        mesPassants.add_thread (new thread (CPassant (temps_avant_rdv, temps_apres_rdv)));
+    }
 
-    #pragma openmp
-    for (thread & philo : philo_qui_ont_faim)
-        philo.join();
-
-    #pragma openmp
-    for (thread & philo : philo_qui_n_ont_plus_faim)
-        philo.join();
-}
-
-int main() {
-
-    exo10();
+    mesPassants.join_all();
 
     return 0;
 }
